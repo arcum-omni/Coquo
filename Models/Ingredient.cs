dbc01953ca5a74b2d2f33475bc31a60890b8d1ba
arcum-omni <travis.l.eiler@gmail.com>
2020-04-08 13:24:07 -0700

    Created Ingredient class

---
+++ Models/Ingredient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coquo.Models
+{
+    public class Ingredient
+    {
+        [Key]
+        [Display(Name = "Ingredient ID")]
+        public int IngredientID { get; set; }
+        [Required]
+        [Display(Name = "Ingredient Name")]
+        public string IngredientName { get; set; }
+
+        [Display(Name = "Ingredient Description")]
+        public string IngredientDescription { get; set; }
+    }
+}
