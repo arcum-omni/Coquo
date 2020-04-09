c5ee6ec24b48a1cc171d6872056aab48963b7422
arcum-omni <travis.l.eiler@gmail.com>
2020-04-08 17:27:30 -0700

    Added migration to include Dish in DB

--- Data/Migrations/20200409001725_Dishes.Designer.cs
+++ Data/Migrations/20200409001725_Dishes.Designer.cs
@@ -4,14 +4,16 @@ using Coquo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Coquo.Data.Migrations
 {
     [DbContext(typeof(KitchenContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200409001725_Dishes")]
+    partial class Dishes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
