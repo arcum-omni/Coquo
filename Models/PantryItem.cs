96a0cb476b5dec4d489d935dfcbfa6d428146df2
arcum-omni <travis.l.eiler@gmail.com>
2020-04-12 12:43:04 -0700

    Created pantry item class

---
+++ Models/PantryItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coquo.Models
+{
+    public class PantryItem
+    {
+        [Key]
+        public int ItemID { get; set; }
+
+        [Required]
+        public Ingredient ItemName { get; set; }
+        
+        public string ItemDescription { get; set; }
+
+        public string ItemVendor { get; set; }
+
+        public double ItemQuantity { get; set; }
+
+        public DateTime DateReceived { get; set; }
+
+        public DateTime? ExpirationDate { get; set; }
+    }
+}
